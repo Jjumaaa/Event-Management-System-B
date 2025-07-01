ed1738ad02f80ecaf284a5140039b4e89131d4b7
Cristina <kamuthucristina@gmail.com>
2025-07-01 21:45:24 +0300

    added the port in app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_restful import Api
+from config import db, bcrypt
+from models import User, Term, Grade, Comment
+from datetime import datetime
+import os
+# from flask_cors import CORS
+
+app = Flask(__name__)
+# CORS(app,
+#      origins=[
+#          "http://localhost:3000",
+#          "https://tima-frontend-7hke.vercel.app"
+#      ],
+#      supports_credentials=True)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tima.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'
+app.secret_key = 'shhh-very-secret'
+
+db.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
+jwt = JWTManager(app)
+
+@app.route('/')
+def notice():
+    return "Hey there, I just want to let you know that this URL is working 💋"
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
